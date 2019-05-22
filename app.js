cb35fef1c6f4dec4e409f6feb6e44638ebe0ff59
Jesper Tejlgaard <jesper@lundogbendsen.dk>
2019-05-22 16:09:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+var allElements = [...document.body.getElementsByTagName('*')];
 
+function findAndReplace(){
+    allElements.forEach(element =>{
+        element.childNodes.forEach(child =>{
+            if(child.nodeType === 3){
+                replaceText(child);
+            }
+        });
+
+    });
+}
+
+function replaceText (node) {
+    let value = node.nodeValue;
+    if (value.includes("Laura Lindahl") && value.includes("")) {
+        value = value.replace(/Laura Lindahl/gi, 'Laura Langpat')
+    }
+
+    node.nodeValue = value;
+}
+
+window.onload = findAndReplace;
